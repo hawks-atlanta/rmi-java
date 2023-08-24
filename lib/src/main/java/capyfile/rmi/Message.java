2136ded6b24e8b986b5c449eade92251179b9573
woynert <woynertgamer@gmail.com>
2023-08-24 11:43:13 -0500

    style: Correct code formatting

--- lib/src/main/java/capyfile/rmi/Message.java
+++ lib/src/main/java/capyfile/rmi/Message.java
@@ -2,12 +2,10 @@ package capyfile.rmi.interfaces;
 
 import java.io.Serializable;
 
-public class Message implements Serializable {
-	
+public class Message implements Serializable
+{
+
 	public String content;
-	
-	public Message(String content) {
-		this.content = content;
-	}
-	
+
+	public Message (String content) { this.content = content; }
 }
--- lib/src/test/java/capyfile/rmi/MessageTest.java
+++ lib/src/test/java/capyfile/rmi/MessageTest.java
@@ -1,12 +1,15 @@
 package capyfile.rmi;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class MessageTest {
-    @Test void someCreationTest() {
-        Message msg = new Message("Hello there");
-        
-        assertTrue(msg.content == "Hello there", "Content should be set");
-    }
+import org.junit.jupiter.api.Test;
+
+class MessageTest
+{
+	@Test void someCreationTest ()
+	{
+		Message msg = new Message ("Hello there");
+
+		assertTrue (msg.content == "Hello there", "Content should be set");
+	}
 }
