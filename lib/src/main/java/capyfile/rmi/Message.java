33a60b1ff4ee2f7ad843818e087ad11ed014f68e
Woynert <61242172+Woynert@users.noreply.github.com>
2023-08-24 12:26:18 -0500

    feat: Simple gradle library project (#1)
    * feat: Base gradle library project
    
    * feat: Add formatting script
    
    * style: Correct code formatting
    
    * style: Specify column limit

---
+++ lib/src/main/java/capyfile/rmi/Message.java
@@ -0,0 +1,11 @@
+package capyfile.rmi.interfaces;
+
+import java.io.Serializable;
+
+public class Message implements Serializable
+{
+
+	public String content;
+
+	public Message (String content) { this.content = content; }
+}
---
+++ lib/src/main/java/capyfile/rmi/MessengerService.java
@@ -0,0 +1,9 @@
+package capyfile.rmi.interfaces;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface MessengerService extends Remote
+{
+	public Message sendMessage (Message clientMessage) throws RemoteException;
+}
