b5b4c294f104be87cb02d7acfd9ce58beb5a4c11
woynert <whayner.porras.2020@upb.edu.co>
2023-09-28 06:04:30 -0500

    feat: Generalize exception type

--- lib/src/main/java/capyfile/rmi/IWorkerService.java
+++ lib/src/main/java/capyfile/rmi/IWorkerService.java
@@ -1,10 +1,11 @@
 package capyfile.rmi;
 
+import java.io.FileNotFoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface IWorkerService extends Remote
 {
 	public void uploadFile (UploadFileArgs args) throws RemoteException;
-	public DownloadFileRes downloadFile (DownloadFileArgs args) throws RemoteException;
+	public DownloadFileRes downloadFile (DownloadFileArgs args) throws FileNotFoundException;
 }
