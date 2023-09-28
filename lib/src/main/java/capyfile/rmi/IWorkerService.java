d5aec4209c7127b54f2ddb2f92fe0b9999de5a0f
woynert <whayner.porras.2020@upb.edu.co>
2023-09-28 12:44:35 -0500

    chore(downloadFile): throw exception

--- lib/src/main/java/capyfile/rmi/IWorkerService.java
+++ lib/src/main/java/capyfile/rmi/IWorkerService.java
@@ -1,11 +1,10 @@
 package capyfile.rmi;
 
-import java.io.FileNotFoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface IWorkerService extends Remote
 {
 	public void uploadFile (UploadFileArgs args) throws RemoteException;
-	public DownloadFileRes downloadFile (DownloadFileArgs args) throws FileNotFoundException;
+	public DownloadFileRes downloadFile (DownloadFileArgs args) throws Exception;
 }
