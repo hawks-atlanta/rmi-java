0a91050da91cdf81df4c1a6d08644a9310a368ab
woynert <whayner.porras.2020@upb.edu.co>
2023-09-27 22:46:11 -0500

    chore: delete sample tests

--- lib/src/test/java/capyfile/rmi/FileTest.java
+++ lib/src/test/java/capyfile/rmi/FileTest.java
@@ -8,9 +8,6 @@ class FileTest
 {
 	@Test void someCreationTest ()
 	{
-		File f = new File ("----", null);
-
-		assertTrue (f.uuid == "----", "UUID should be set");
-		assertTrue (f.contents == null, "Contents should be set");
+		assertTrue (true, "demo msg");
 	}
 }
